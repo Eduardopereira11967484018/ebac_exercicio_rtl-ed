1f976c9b5feaa28df86ead25991771d5939ceaf2
Eduardopereira11967484018 <eduardoperira.lima@yahoo.com.br>
2024-07-16 08:50:54 -0300

    Corrigir propriedade altText ausente e nome correto da propriedade imageUrl no componente Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Post from "./components/Post";
-
+import Post from "./components/Post/index";
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post 
+        imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg" 
+        altText="Miniatura do Batmóvel de 1989"
+      >
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
